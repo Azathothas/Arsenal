dd8a9a5d42f766e76c6071026656f2c2c75c69ff
Ajam <AjamX101@gmail.com>
2023-09-17 08:39:57 +0545

    Update main.go

--- certstream/main.go
+++ certstream/main.go
@@ -14,25 +14,25 @@ func main() {
 	defer w.Flush()
 
 	// The false flag specifies that we want heartbeat messages.
-  // Default timeou is ~ 30-60 Seconds
+	// Default timeout is ~ 30-60 Seconds
 	stream, errStream := certstream.CertStreamEventStream(false)
 	for {
 		select {
-    //Use JQ, to parse only Domains  
+		// Use JQ, to parse only Domains
 		case jq := <-stream:
-      //Same as: certstream --full --json | jq -r '.data.leaf_cert.all_domains[]'
+			// Same as: certstream --full --json | jq -r '.data.leaf_cert.all_domains[]'
 			domains, err := jq.ArrayOfStrings("data", "leaf_cert", "all_domains")
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Error decoding json", err)
 			} else {
 				for _, domain := range domains {
-          //Removes Wildcards(.*) with empties (""), but only for first occurance in a line (1)
+					// Removes Wildcards(.*) with empties (""), but only for the first occurrence in a line (1)
 					fmt.Fprintln(w, strings.Replace(domain, "*.", "", 1))
 				}
 			}
 		case err := <-errStream:
-      //Uncomment, if std errors are to be printed as std out
-			//fmt.Fprintln(os.Stderr, "Stream error", err)
+			// Uncomment, if std errors are to be printed as std out
+			// fmt.Fprintln(os.Stderr, "Stream error", err)
 		}
 	}
 }
