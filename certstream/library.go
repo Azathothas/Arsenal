36209a77d457f8eabb1c6cf4e26f3ad3064968c1
Ajam <AjamX101@gmail.com>
2023-12-06 11:41:56 +0545

    Create library.go

---
+++ certstream/library.go
@@ -0,0 +1,83 @@
+// https://github.com/CaliDog/certstream-go/issues/10
+// https://github.com/pathtofile/certstream-go/blob/master/certstream.go
+package certstream
+
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/jmoiron/jsonq"
+	"github.com/pkg/errors"
+)
+
+const (
+	pingPeriod time.Duration = 15 * time.Second
+)
+
+func CertStreamEventStreamURL(skipHeartbeats bool, url string) (chan jsonq.JsonQuery, chan error) {
+	outputStream := make(chan jsonq.JsonQuery)
+	errStream := make(chan error)
+
+	go func() {
+		for {
+			c, _, err := websocket.DefaultDialer.Dial(url, nil)
+
+			if err != nil {
+				errStream <- errors.Wrap(err, "Error connecting to certstream! Sleeping a few seconds and reconnecting... ")
+				time.Sleep(5 * time.Second)
+				continue
+			}
+
+			defer c.Close()
+			defer close(outputStream)
+
+			done := make(chan struct{})
+
+			go func() {
+				ticker := time.NewTicker(pingPeriod)
+				defer ticker.Stop()
+
+				for {
+					select {
+					case <-ticker.C:
+						c.WriteMessage(websocket.PingMessage, nil)
+					case <-done:
+						return
+					}
+				}
+			}()
+
+			for {
+				var v interface{}
+				c.SetReadDeadline(time.Now().Add(15 * time.Second))
+				err = c.ReadJSON(&v)
+				if err != nil {
+					errStream <- errors.Wrap(err, "Error decoding json frame!")
+					c.Close()
+					break
+				}
+
+				jq := jsonq.NewQuery(v)
+
+				res, err := jq.String("message_type")
+				if err != nil {
+					errStream <- errors.Wrap(err, "Could not create jq object. Malformed json input recieved. Skipping.")
+					continue
+				}
+
+				if skipHeartbeats && res == "heartbeat" {
+					continue
+				}
+
+				outputStream <- *jq
+			}
+			close(done)
+		}
+	}()
+
+	return outputStream, errStream
+}
+
+func CertStreamEventStream(skipHeartbeats bool) (chan jsonq.JsonQuery, chan error) {
+	return CertStreamEventStreamURL(skipHeartbeats, "wss://certstream.calidog.io")
+}
