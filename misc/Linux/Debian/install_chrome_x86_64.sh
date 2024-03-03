5b1a3ca00c8608a2e7b2e3a5d6c334764b1499f1
Ajam <AjamX101@gmail.com>
2024-03-03 10:17:12 +0545

    Update install_chrome_x86_64.sh

--- misc/Linux/Debian/install_chrome_x86_64.sh
+++ misc/Linux/Debian/install_chrome_x86_64.sh
@@ -4,10 +4,13 @@
 # bash <(curl -qfsSL "https://pub.ajam.dev/repos/Azathothas/Arsenal/misc/Linux/Debian/install_chrome_x86_64.sh")
 
 ##Google Chrome
+echo -e "\n[+] Installing Google Chrome\n"
 pushd "$(mktemp -d)" > /dev/null 2>&1 && curl -qfsSL "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -o "/tmp/chrome.deb"
 sudo apt install "/tmp/chrome.deb" -y
+echo -e "\n[+] Updating Browser Components\n"
 sudo apt update -y && sudo apt upgrade -y ; popd > /dev/null 2>&1 ; google-chrome --version
 ##Chromium : https://github.com/phd/chromium-repo | https://freeshell.de/phd/chromium/
+echo -e "\n[+] Installing Chromium\n"
 source "/etc/lsb-release"
 echo "deb https://freeshell.de/phd/chromium/${DISTRIB_CODENAME} /" | sudo tee "/etc/apt/sources.list.d/phd-chromium.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "869689FE09306074"
