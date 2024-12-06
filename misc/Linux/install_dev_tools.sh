4095ba97a81b99fb306361c6255601c9586d19d1
Ajam <AjamX101@gmail.com>
2024-12-06 11:26:49 +0545

    notify

--- misc/Linux/install_dev_tools.sh
+++ misc/Linux/install_dev_tools.sh
@@ -183,6 +183,8 @@ INSTALL_DIR_SIZE_PRE="$(du -sh ${INSTALL_DIR} | cut -f1)" && export INSTALL_DIR_
  eval "${INSTALL_PRE}" "${INSTALL_SRC}/miniserve" -o "${INSTALL_DIR}/miniserve" && eval "${INSTALL_POST}" "${INSTALL_DIR}/miniserve" "${INSTALL_STRATEGY}"
 #ncdu : https://dev.yorhel.nl/ncdu
  eval "${INSTALL_PRE}" "${INSTALL_SRC}/ncdu" -o "${INSTALL_DIR}/ncdu" && eval "${INSTALL_POST}" "${INSTALL_DIR}/ncdu" "${INSTALL_STRATEGY}"
+#notify: https://github.com/projectdiscovery/notify
+ eval "${INSTALL_PRE}" "${INSTALL_SRC}/notify" -o "${INSTALL_DIR}/notify" && eval "${INSTALL_POST}" "${INSTALL_DIR}/notify" "${INSTALL_STRATEGY}"
 #ouch : https://github.com/ouch-org/ouch
  eval "${INSTALL_PRE}" "${INSTALL_SRC}/ouch" -o "${INSTALL_DIR}/ouch" && eval "${INSTALL_POST}" "${INSTALL_DIR}/ouch" "${INSTALL_STRATEGY}"
 #pipetty: https://github.com/kilobyte/colorized-logs
